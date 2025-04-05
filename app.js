bfa0e171a0b238632179642a8e11885feb3cf66e
Dhinakarkandhasamy <kdhinakar15@gmail.com>
2025-04-04 23:10:35 -0700

    kajka

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ var qPaper = document.getElementById("q-paper").addEventListener('click',functio
 var sem1 = document.getElementById("sem1").addEventListener("click",function(){
     document.querySelector(".subject-container").style.display="flex"
 })
+var sem2 = document.getElementById("sem2").addEventListener("click",function(){
+    document.querySelector(".subject-container").style.display="flex"
+})
+var sem3 = document.getElementById("sem3").addEventListener("click",function(){
+    document.querySelector(".subject-container").style.display="flex"
+})
+var sem4 = document.getElementById("sem4").addEventListener("click",function(){
+    document.querySelector(".subject-container").style.display="flex"
+})
+var sem5 = document.getElementById("sem5").addEventListener("click",function(){
+    document.querySelector(".subject-container").style.display="flex"
+})
+var sem6 = document.getElementById("sem6").addEventListener("click",function(){
+    document.querySelector(".subject-container").style.display="flex"
+})
